814f9ecdcbb1aa2f0039e58dfcb73bc57bbc3ce8
K1GQ <K1GQ@mac.com>
2018-06-12 18:39:43 -0400

    Set target OS for ORSSerialPort to 10.10 Delete WinKeyerHostModePreferences.xib -- unused

---
+++ WinKeyerHostModePreferences.h
@@ -0,0 +1,14 @@
+//
+//  WinKeyerHostModePreferences.h
+//  MacWinKeyer
+//
+//  Created by Willard Myers on 2017-04-20.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+@import CCNPreferencesWindowController;
+
+@interface WinKeyerHostModePreferences : NSViewController <CCNPreferencesWindowControllerProtocol>
+
+@end
