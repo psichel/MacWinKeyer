092c58899c553e8e2b0bd271456668ddbe4790fe
Peter Sichel <psichel@sustworks.com>
2020-10-17 16:06:20 -0400

    Initial project received from Bill Myers, K1GQ

---
+++ Build/Intermediates/ORSSerialPort.build/Debug/ORSSerial.build/DerivedSources/ORSSerial_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char ORSSerialVersionString[];
+ extern const double ORSSerialVersionNumber;
+
+ const unsigned char ORSSerialVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:ORSSerial  PROJECT:ORSSerialPort-365" "\n";
+ const double ORSSerialVersionNumber __attribute__ ((used)) = (double)365.;
